7b623a26ef08bf5b7be2bbbae70cc877cdab1632
Ali Ahmed <tombstone.act@gmail.com>
2022-10-01 16:56:20 +0500

    New Comp

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,21 @@
+import ExpenseDate from './ExpenseDate'
 import "./ExpenseItem.css";
+
 
 function ExpenseItem(props) {
 
+
+
     return (
         <div className="expense-item">
-            <div>
-                {/* to localte String helps to output date in human readable method */ }
-                <div>{ props.date.toLocaleString('en-US', { month: 'long' }) }</div>
-                <div>Year</div>
-                <div>Day</div>
-            </div>
+
+            <ExpenseDate date={ props.date }></ExpenseDate>
+
             <div className="expense-item__description">
                 <h2>{ props.title }</h2>
                 <div className="expense-item__price">$ { props.amount }</div>
             </div>
+
         </div>
     );
 }
