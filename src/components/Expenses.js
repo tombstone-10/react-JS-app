ee5d9fa71935514bbd51c6160663d47932fece35
Ali Ahmed <tombstone.act@gmail.com>
2022-10-01 17:56:56 +0500

    Card Comp
    Card component generated for optimisation

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./ExpenseItem"
+import Card from "./Card"
 import "./Expenses.css"
 
 
@@ -6,14 +7,14 @@ function Expesnes(props) {
 
     return (
 
-        <div className="expenses">
+        <Card className="expenses">
 
             <ExpenseItem title={ props.items[0].title } amount={ props.items[0].amount } date={ props.items[0].date }></ExpenseItem>
             <ExpenseItem title={ props.items[1].title } amount={ props.items[1].amount } date={ props.items[1].date }></ExpenseItem>
             <ExpenseItem title={ props.items[2].title } amount={ props.items[2].amount } date={ props.items[2].date }></ExpenseItem>
             <ExpenseItem title={ props.items[3].title } amount={ props.items[3].amount } date={ props.items[3].date }></ExpenseItem>
 
-        </div>
+        </Card>
 
     );
 
